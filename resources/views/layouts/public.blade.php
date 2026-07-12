c8bfaaad2a26f1c6be065710726e8e9a64346229
WouterMeens <w.m.meens@gmail.com>
2026-07-12 16:05:28 +0200

    update

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -26,6 +26,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="rL2igVYswJn8aNcCv9m8jOerGBKanqMJrygtW_aPxk8" />
     <title>{{ $title }}</title>
     <meta name="description" content="{{ $description }}">
     <meta name="robots" content="{{ $robots }}">
